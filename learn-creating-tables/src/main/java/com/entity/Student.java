978afb64cac2389a00bc36489abf5dcc77487a6f
shakthi <shakthisravanth001@gmail.com>
2026-01-07 12:27:56 +0530

    Lab - 2 : Initial commit

---
+++ learn-creating-tables/src/main/java/com/entity/Student.java
@@ -0,0 +1,38 @@
+package com.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String name;
+	private int age;
+
+	public Student() {
+		
+	}
+
+	public Student(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+}
