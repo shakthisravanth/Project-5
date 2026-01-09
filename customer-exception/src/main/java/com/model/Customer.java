40334e9b0be327a599ea74ad2767cbc5e47f0b43
J SHAKTHI SRAVANTH <121029982+shakthisravanth@users.noreply.github.com>
2026-01-09 11:50:30 +0530

    Add ID generation strategy to Customer entity

--- customer-exception/src/main/java/com/model/Customer.java
+++ customer-exception/src/main/java/com/model/Customer.java
@@ -2,11 +2,14 @@ package com.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
 @Entity
 public class Customer {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
 	private String email;
